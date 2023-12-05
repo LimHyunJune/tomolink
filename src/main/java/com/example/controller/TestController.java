2ab9b34218f775300f64fe3a5b36a88337b60275
Imu <48524793+LimHyunJune@users.noreply.github.com>
2023-12-05 21:32:59 +0900

    add template 추가

--- src/main/java/com/example/controller/TestController.java
+++ src/main/java/com/example/controller/TestController.java
@@ -2,6 +2,7 @@ package com.example.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -14,9 +15,22 @@ public class TestController {
         return "index";
     }
 
-    @GetMapping("/friends")
+    @GetMapping("friends")
     public String friends()
     {
         return "friends";
+    }
+
+    @GetMapping("add")
+    public String addForm()
+    {
+        return "add";
+    }
+
+    @PostMapping("add")
+    public String addPost()
+    {
+        // PRG Pattern
+        return "redirect:/friends";
     }
 }
