75506efd1d70de4e37a1994dc6459afee7dcffc3
Imu <48524793+LimHyunJune@users.noreply.github.com>
2023-12-07 22:24:50 +0900

    Member logic

---
+++ src/main/java/com/example/entity/Member.java
@@ -0,0 +1,35 @@
+package com.example.entity;
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotEmpty;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class Member {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long id;
+
+    @NotEmpty
+    String name;
+
+    @NotEmpty
+    @Email
+    String email;
+
+    @NotEmpty
+    String password;
+
+}
