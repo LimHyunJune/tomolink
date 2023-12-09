5fc147fd13886de956b1ab76a194974d6a618ef1
Imu <48524793+LimHyunJune@users.noreply.github.com>
2023-12-09 23:36:45 +0900

    multi check 라디오 변경

--- src/main/java/com/example/friend_search/dto/FriendSearchPostForm.java
+++ src/main/java/com/example/friend_search/dto/FriendSearchPostForm.java
@@ -1,15 +1,21 @@
 package com.example.friend_search.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 
 @Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class FriendSearchPostForm {
 
     String title;
     String contents;
-    List<String> postTypes;
+    String tag;
 
 }
 
