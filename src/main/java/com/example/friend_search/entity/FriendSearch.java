36c7c7607ad59f597091baff41140336c5161694
Imu <48524793+LimHyunJune@users.noreply.github.com>
2023-12-10 21:10:27 +0900

    LocalDateTime converter 적용

--- src/main/java/com/example/friend_search/entity/FriendSearch.java
+++ src/main/java/com/example/friend_search/entity/FriendSearch.java
@@ -5,7 +5,9 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.format.annotation.DateTimeFormat;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Entity
@@ -22,4 +24,8 @@ public class FriendSearch {
     String title;
     String contents;
     String tag;
+    Long memberId;
+
+    @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    LocalDateTime createdAt;
 }
