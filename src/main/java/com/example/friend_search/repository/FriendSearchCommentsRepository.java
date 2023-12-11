ec79f079cfae541e11723f3581ddbff385831712
Imu <48524793+LimHyunJune@users.noreply.github.com>
2023-12-11 22:16:38 +0900

    FriendSearchCommentsRepository 구현

---
+++ src/main/java/com/example/friend_search/repository/FriendSearchCommentsRepository.java
@@ -0,0 +1,12 @@
+package com.example.friend_search.repository;
+
+import com.example.friend_search.entity.FriendSearchComments;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface FriendSearchCommentsRepository extends JpaRepository<FriendSearchComments, Long> {
+    List<FriendSearchComments> findByPostId(Long postId);
+}
