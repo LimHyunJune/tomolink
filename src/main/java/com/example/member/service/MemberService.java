f6e0f5a3b0c279d90e54791b209ef119a6386778
Imu <48524793+LimHyunJune@users.noreply.github.com>
2023-12-11 22:17:12 +0900

    구현 추가

--- src/main/java/com/example/member/service/MemberService.java
+++ src/main/java/com/example/member/service/MemberService.java
@@ -6,6 +6,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.Optional;
+
 @Service
 @RequiredArgsConstructor
 public class MemberService {
@@ -21,5 +23,9 @@ public class MemberService {
     public Member findByEmail(String email)
     {
         return memberRepository.findByEmail(email);
+    }
+
+    public Optional<Member> findById(Long memberId) {
+        return memberRepository.findById(memberId);
     }
 }
