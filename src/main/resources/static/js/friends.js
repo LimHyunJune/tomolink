e51aff4e93ba3e1ebf45e6653dc212b3236c3098
Imu <48524793+LimHyunJune@users.noreply.github.com>
2023-12-14 22:56:35 +0900

    시간 순 내림차순 기본 정렬
    friend-search-board
    friend-search-post comments

--- src/main/resources/static/js/friends.js
+++ src/main/resources/static/js/friends.js
@@ -1,17 +1,46 @@
 $( document ).ready(function() {
     $('#posts').DataTable( {
-       ordering:  false,
+       ordering:  true,
+       order: [[3, 'desc']],
        pageLength : 7,
        info : false,
        lengthChange : false
     } );
 
     $('#comments').DataTable( {
-           ordering:  false,
+           ordering:  true,
+           order: [[2, 'desc']],
            pageLength : 10,
            info : false,
            searching : false,
            lengthChange : false
         } );
+
+    if(scroll)
+    {
+        window.scrollTo(window.scrollX,scroll);
+    }
 });
 
+
+function setCookie(name, value = "", expireDays = "", path = "/") {
+  let expires = "";
+  if (expireDays) {
+    let date = new Date();
+    date.setTime(date.getTime() + expireDays * 24 * 60 * 60 * 1000);
+    expires = `expires=${date.toUTCString()};`;
+  }
+  document.cookie = `${name}=${value || ""};${expires}path=${path}`;
+}
+
+function friendSearchCommentsSecretSubmit()
+{
+    setCookie("scroll", window.scrollY);
+    $('#secret_submit').submit();
+}
+
+function friendSearchCommentsNoSecretSubmit()
+{
+    setCookie("scroll", window.scrollY);
+    $('#no_secret_submit').submit();
+}
